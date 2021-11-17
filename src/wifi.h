054df546e0374b3c06eeac260ed74cdca8d758cb
lisboaigor <igorlisboa30@hotmail.com>
2021-11-17 16:55:40 -0300

    first commit

---
+++ src/wifi.h
@@ -0,0 +1,31 @@
+#include <string.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <freertos/event_groups.h>
+#include <esp_system.h>
+#include <esp_wifi.h>
+#include <esp_event.h>
+#include <esp_log.h>
+#include <nvs_flash.h>
+
+#include <lwip/err.h>
+#include <lwip/sys.h>
+
+#define CONFIG_ESP_WIFI_SSID "***********"     // Nome da rede
+#define CONFIG_ESP_WIFI_PASSWORD "***********" // Senha
+#define CONFIG_ESP_MAXIMUM_RETRY 10
+
+// Sinalizador de evento de conexão
+static EventGroupHandle_t s_wifi_event_group;
+
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT BIT1
+
+static const char *TAG = "Controle de porta - WIFI: ";
+
+static int s_retry_num = 0;
+
+static void event_handler(void *arg, esp_event_base_t event_base,
+                          int32_t event_id, void *event_data);
+
+void wifi_init_sta(void);
