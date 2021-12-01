f190a42b3a18f6e147d9e751adc848b5e35d36bc
Joao Gustavo <joao.carvalho@dcomp.ufs.br>
2021-12-01 16:07:02 -0300

    Adicionado comando de parada.

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,17 @@
 #include "wifiConfig.h"
 
 // Definições do Telegram
-#define telegram_token "2103501476:AAGVqUHNVZ8r1xIIp2-uc07p0PDlMHDalg0"
+#define telegram_token "5095603270:AAEK5LtloyQ_wdCXKjZFqugyaVIJjeA6p_8"
 #define TIME_MSG_CHECK 1000 // Checa se há mensagens a cada 1 segundo
 
 // Definições das mensagens possíveis de serem recebidas
-#define OPEN_GATE_CMD "/abrir"
+#define OPEN_GATE_CMD  "/abrir"
 #define CLOSE_GATE_CMD "/fechar"
+#define STOP_GATE_CMD  "/parar"
 
 // Status do portão
-bool status;
+uint8_t status;
+uint8_t engine_loops;
 
 // Variáveis do usadas pelo bot
 String msg, id;
@@ -44,11 +46,31 @@ void setupWiFi()
     Serial.println(WiFi.localIP());
 }
 
-void openGate(String id)
+bool getStopCmd(uint32_t *msg_amount) 
+{
+    *msg_amount = bot.getUpdates(bot.last_message_received + 1);
+
+    for (uint32_t i = 0; i < *msg_amount; i++)
+    {
+        id  = bot.messages[i].chat_id;
+        msg = bot.messages[i].text;
+
+        if (msg.equals(STOP_GATE_CMD) && status == false) 
+        {
+            status = 0;
+            bot.sendMessage(id, "Portão parado.");
+
+            return true;
+        }
+    }
+    return false;
+}
+
+void openGate(String id, uint32_t *msg_amount)
 {
     bot.sendMessage(id, "Abrindo o portão...");
     
-    for (uint8_t i = 0; i < 6; i++)
+    for (uint8_t i = engine_loops; i <= 12; i++)
     {
         digitalWrite(LED_BUILTIN, HIGH);
         delay(500);
@@ -56,16 +78,21 @@ void openGate(String id)
         digitalWrite(LED_BUILTIN, LOW);
         delay(500);
         Serial.println("LED OFF");
+
+        engine_loops++;
+
+        if (getStopCmd(msg_amount))
+            return;
     }
 
-    status = true;
+    status = 1;
     bot.sendMessage(id, "Portão aberto!");
 }
 
-void closeGate(String id)
+void closeGate(String id, uint32_t *msg_amount)
 {
     bot.sendMessage(id, "Fechando o portão...");
-    for (uint8_t i = 0; i < 12; i++)
+    for (uint8_t i = engine_loops; i >= 0; i--)
     {
         digitalWrite(LED_BUILTIN, HIGH);
         delay(250);
@@ -73,9 +100,14 @@ void closeGate(String id)
         digitalWrite(LED_BUILTIN, LOW);
         delay(250);
         Serial.println("LED OFF");
+
+        engine_loops++;
+
+        if (getStopCmd(msg_amount))
+            return;
     }
 
-    status = false;
+    status = -1;
     bot.sendMessage(id, "Portão fechado!");
 }
 
@@ -83,15 +115,15 @@ void read_messages(uint32_t *msg_amount)
 {
     for (uint32_t i = 0; i < *msg_amount; i++)
     {
-        id = bot.messages[i].chat_id;
+        id  = bot.messages[i].chat_id;
         msg = bot.messages[i].text;
 
         if (msg.equals(OPEN_GATE_CMD) && status == false)
-            openGate(id);
+            openGate(id, msg_amount);
         else if (msg.equals(OPEN_GATE_CMD) && status == true)
             bot.sendMessage(id, "O portão já está aberto!");
         else if (msg.equals(CLOSE_GATE_CMD) && status == true)
-            closeGate(id);
+            closeGate(id, msg_amount);
         else if (msg.equals(CLOSE_GATE_CMD) && status == false)
             bot.sendMessage(id, "O portão já está fechado!");
         else
@@ -103,6 +135,9 @@ void setup()
 {
     Serial.begin(115200);
     pinMode(LED_BUILTIN, OUTPUT);
+    
+    status = -1;
+    engine_loops = 0;
 
     setupWiFi();
 }
